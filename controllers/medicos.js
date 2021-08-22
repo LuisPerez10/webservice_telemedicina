c5af291b09433be5ba520c5272858610c95f2280
Franklin <franklin.ojc@gmail.com>
2021-08-21 22:38:39 -0400

    endpoint buscarmedico

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -2,8 +2,9 @@ const { response } = require('express');
 
 const Medico = require('../models/medico');
 const Persona = require('../models/persona');
+const Usuario = require('../models/usuario');
 
-var  listamedicos = [];
+
 
 const getMedicos = async(req, res = response) => {
 
@@ -164,45 +165,103 @@ const getMedicoByEspecialidad = async(req, res = response) => {
 }
 
 
+// const getMedicoByNombre = async(req, res = response) => {
+
+//     const nombre = req.body.nombre;
+//      const regexName = new RegExp(nombre, 'i');
+
+//     try {
+//         const   personasDB = await Persona.find( { nombre: regexName}, 'nombre apellido').sort('nombre')
+//            .populate({path: 'usuario',  match: {role:"MEDICO_ROLE", estado: "habilitado"}, select: 'estado email role id'});
+
+         
+//       if (!personasDB) {
+//           return res.status(404).json({
+//               ok: false,
+//               msg: 'No existe una persona por ese nombre'
+//           });
+//       }
+ 
+//    var listamedicos = [];  
+//    if(personasDB.length == 0){
+//     return res.status(200).json({
+//         ok: false,
+//         listamedicos
+//     });
+//    }
+//        //console.log(personasDB);
+//     var index = 0;
+
+
+//    var data = {
+//         usuario:usuario,
+//         persona:personaDB
+//     }
+//     personasDB.forEach( async(data) =>  {
+        
+//         if(personasDB[index].usuario){
+//             const medico =  await Medico.findOne({ "persona": data._id}, 'calificacion especialidad descripcion')
+//             .populate({path: 'persona', select: 'nombre apellido', populate:{path:'usuario', select: 'img email'}} );   
+//             console.log(medico);
+//             if(medico ){
+//                 listamedicos.push(medico);
+//             }
+//         }
+//         index = index + 1;
+       
+//             if(index == (personasDB.length )){
+
+//                 return  res.json({
+//                     ok: true,
+//                     listamedicos,
+//                 });
+//             }
+        
+//       });
+//   } catch (error) {
+//       console.log(error);
+//       res.status(500).json({
+//           ok: false,
+//           msg: 'Error inesperado'
+//       })
+//   }
+
+  
+// }
+
 const getMedicoByNombre = async(req, res = response) => {
 
     const nombre = req.body.nombre;
      const regexName = new RegExp(nombre, 'i');
 
     try {
-        const   personasDB = await Persona.find( { nombre: regexName}, 'nombre apellido').sort('nombre')
-           .populate({path: 'usuario',  match: {role:"MEDICO_ROLE", estado: "habilitado"}, select: 'estado email role id'});
+        const usuarioDB = await Usuario.find(
+             { nombre: regexName, role: 'MEDICO_ROLE', estado: "habilitado"}, 
+             'nombre img email').sort('nombre');
+       
 
-         
-      if (!personasDB) {
-          return res.status(404).json({
-              ok: false,
-              msg: 'No existe una persona por ese nombre'
-          });
-      }
- 
    var listamedicos = [];  
-   if(personasDB.length == 0){
-    return res.status(200).json({
-        ok: false,
-        listamedicos
-    });
-   }
-       console.log(personasDB);
     var index = 0;
-    personasDB.forEach( async(data) =>  {
+
+        usuarioDB.forEach( async(usuario) =>  {
         
-        if(personasDB[index].usuario){
-            const medico =  await Medico.findOne({ "persona": data._id}, 'calificacion especialidad descripcion')
-            .populate({path: 'persona', select: 'nombre apellido'});    
+            const personaDB =  await Persona.findOne({ "usuario": usuario._id}, 'nombre apellido celular');
+
+            const medicoDB =  await Medico.findOne({ "persona": personaDB._id}, 'calificacion especialidad descripcion');
             
-            if(medico ){
-                listamedicos.push(medico);
-            }
-        }
+           // console.log(medicoDB);
+                var data = {
+                    usuario:usuario,
+                    persona:personaDB,
+                    medico:medicoDB
+                }
+
+                listamedicos.push(data);
+           console.log(listamedicos);
+        
         index = index + 1;
        
-            if(index == (personasDB.length )){
+            if(index == (usuarioDB.length )){
 
                 return  res.json({
                     ok: true,
